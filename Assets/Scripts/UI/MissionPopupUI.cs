d4802da40fadb1a76e7336e2a501fe323154e63c
TheFoolUpright <mcenteemaryk@gmail.com>
2026-03-22 11:03:43 +0000

    Canvas

--- Assets/Scripts/UI/MissionPopupUI.cs
+++ Assets/Scripts/UI/MissionPopupUI.cs
@@ -8,7 +8,10 @@ public class MissionPopupUI : MonoBehaviour
     [SerializeField] private TMP_Text missionTitleText;
     [SerializeField] private TMP_Text missionDescriptionText;
     [SerializeField] private Image missionImage;
-    [SerializeField] private TMP_Text professionalismText;
+
+    [SerializeField] private Image[] starImages;
+    [SerializeField] private Color activeColor = Color.yellow;
+    [SerializeField] private Color inactiveColor = Color.white;
 
     [SerializeField] private PigSlot[] missionSlots;
 
@@ -32,7 +35,7 @@ public class MissionPopupUI : MonoBehaviour
         missionTitleText.text = mission.missionsScriptableObject.missionTitle;
         missionDescriptionText.text = mission.missionsScriptableObject.missionDescription;
         missionImage.sprite = mission.missionsScriptableObject.missionImage;
-        professionalismText.text = "Suggested: " + mission.missionsScriptableObject.missionProfessionalismRequirement;
+        UpdateStars(Mathf.RoundToInt(mission.missionsScriptableObject.missionProfessionalismRequirement));
 
         for (int i = 0; i < missionSlots.Length; i++)
         {
@@ -45,5 +48,20 @@ public class MissionPopupUI : MonoBehaviour
     {
         popupRoot.SetActive(false);
         currentMission = null;
+    }
+
+    void UpdateStars(int professionalism)
+    {
+        for (int i = 0; i < starImages.Length; i++)
+        {
+            if (i < professionalism)
+            {
+                starImages[i].color = activeColor;
+            }
+            else
+            {
+                starImages[i].color = inactiveColor;
+            }
+        }
     }
 }
