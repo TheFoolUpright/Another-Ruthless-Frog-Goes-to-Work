4c5c23b42d37a2e470ad85dd3242fa16b6d4d555
EdyPEN <edy2.0andre@gmail.com>
2026-03-22 10:58:01 +0000

    Missions Pop-Up when clicked

--- Assets/Scripts/Missions/MissionClicking.cs
+++ Assets/Scripts/Missions/MissionClicking.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class MissionClicking : MonoBehaviour
 {
+    public GameObject missionPopUpUIObject;
+    public MissionPopupUI missionPopupUI;
+    public MissionInfo missionInfo;
+
+    void Start()
+    {
+        missionInfo = GetComponent<MissionInfo>();
+        missionPopUpUIObject = missionInfo.missionPopUpUIObject;
+    }
+
     void OnMouseDown()
     {
-        Debug.Log("Clicked!");
+        Debug.Log(missionInfo);
+        missionPopUpUIObject.SetActive(true);
+        missionPopUpUIObject.GetComponent<MissionPopupUI>().Open(missionInfo);
     }
 }
