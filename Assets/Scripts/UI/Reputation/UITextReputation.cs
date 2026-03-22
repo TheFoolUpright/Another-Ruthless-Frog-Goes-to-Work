4b9ea7cdb3b1c2cffae61a0f3806343f7d8a84b7
Duarte20002 <duartesaopedro@gmail.com>
2026-03-22 00:07:49 +0000

    Made an Everything Scene

--- Assets/Scripts/UI/Reputation/UITextReputation.cs
+++ Assets/Scripts/UI/Reputation/UITextReputation.cs
@@ -31,6 +31,6 @@ public class UITextReputation : MonoBehaviour
             return;
         }
 
-        reputationText.text = "%" + zonePrefab.zone.zoneReputation.ToString();
+        reputationText.text = zonePrefab.zone.zoneReputation.ToString() + "%";
     }
 }
